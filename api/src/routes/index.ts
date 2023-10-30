c540ab6e991ba1f79a9d1d1abc62fc307427a9d6
Naoto Hori <naoto.hori@gmail.com>
2023-10-31 05:41:04 +0900

    Shortest Path Graph And API

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import moviesRouter from './movies';
 import peopleRouter from './people';
+import shortestPathRouter from './shortestPath';
 import visualizationRouter from './visualization';
-
 const routers = Router();
 
 routers.use('/movies', moviesRouter);
 routers.use('/people', peopleRouter);
 routers.use('/visualization', visualizationRouter);
+routers.use('/shortest-path', shortestPathRouter);
 
 export default routers;
